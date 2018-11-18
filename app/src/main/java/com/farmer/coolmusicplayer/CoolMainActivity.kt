320c1a4651bfc175e665a56ea5ddb455382e9760
Vincent Wang <wmtcc@163.com>
2018-11-18 13:53:28 +0800

    initial repositories

---
+++ app/src/main/java/com/farmer/coolmusicplayer/CoolMainActivity.kt
@@ -0,0 +1,12 @@
+package com.farmer.coolmusicplayer
+
+import android.app.Activity
+import android.os.Bundle
+
+class CoolMainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cool_main_layout)
+    }
+}
